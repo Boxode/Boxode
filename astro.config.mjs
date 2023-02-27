42998cbabd325e869ec4e0ae7f5b3d86879fab67
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-02-27 01:23:17 -0600

    🆕 vercel CLI added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import react from '@astrojs/react'
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import mdx from '@astrojs/mdx'
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
-})
+  integrations: [tailwind(), react(), mdx()],
+  output: "server",
+  adapter: vercel()
+});
