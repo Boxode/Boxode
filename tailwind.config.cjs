44f9df14cd70d1ee46887514f591623084596c82
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-15 00:00:49 -0600

    🆕 animation added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
 		'./src/pages/opensource/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./src/pages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				blob: 'blob 7s infinite',
+			},
+			keyframes: {
+				blob: {
+					"0%": { transform: "translate(0px, 0px) scale(1)"},
+					"33%": { transform: "translate(30px, -50px) scale(1.1)"},
+					"66%": { transform: "translate(-20px, 20px) scale(0.9)"},
+					"100%": { transform: "translate(0px, 0px) scale(1)"},
+				}
+			}
+		},
 	},
 	plugins: [],
 }
