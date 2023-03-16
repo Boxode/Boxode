70ad80b4496186b7b7f7ab3469519c7649107358
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-16 00:06:32 -0600

    🆕 about component added

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,14 @@
 const AboutUs = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-    </div>
+  <section id='about'>
+	<div>
+	  <div className='flex items-center justify-center flex-col mt-32 mb-10'>
+		<img src="/2.webp" className='w-12 select-none' alt="2" />
+		<h2 className='about text-6xl lg:text-8xl font-[SegoeBold] tracking-normal p-4 opacity-90 select-none'>Nosotros</h2>
+		<p className='text-gray-100/50 text-md px-6 text-center lg:text-2xl font-[Questrial]'>Te dejamos un poco de información relevante sobre nuestra misión y visión.</p>
+	  </div>
+	</div>
+  </section>
   )
 }
 
