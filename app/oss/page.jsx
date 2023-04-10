c5ae0e1c00225647ae9be6f64637d8124480be80
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-04-09 23:31:25 -0600

    🔌 HomeOSS component linked

--- app/oss/page.jsx
+++ app/oss/page.jsx
@@ -1,9 +1,11 @@
 import HeaderOSS from '@/components/OSS/Header'
+import HomeOSS from '@/components/OSS/Home'
 
 export default function OSS() {
   return (
     <>
       <HeaderOSS />
+      <HomeOSS />
     </>
   )
 }
