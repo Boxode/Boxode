d2eb6c0c50dc0c61f188c9ae1f3c0a4d8f0e9667
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-04-19 23:00:32 -0600

    🆕 Projects components added

---
+++ components/OSS/Projects.jsx
@@ -0,0 +1,9 @@
+export default function Projects () {
+  return (
+    <section>
+      <div>
+        <p className='text-white font-[SegoeBold] text-center'>Projects</p>
+      </div>
+    </section>
+  )
+}
