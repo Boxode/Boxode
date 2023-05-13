17cd9a150eed39eddb1a3943a939855682936a63
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-05-12 23:57:32 -0600

    🛠️ fix the image route

--- app/not-found.js
+++ app/not-found.js
@@ -8,35 +8,35 @@ export default function NotFound() {
   return (
     <>
       <Header />
-      <section class='section-error min-h-screen stars'>
+      <section className='section-error min-h-screen stars'>
         <Image
-          src='/404.svg'
+          src='/svg/404.svg'
           alt='404'
           class='img-error'
           width={500}
           height={500}
         />
         <div class='font-[SegoeBold] pt-[5%] text-center'>
-          <span id='L' class='error flex-row'>
+          <span id='L' className='error flex-row'>
             4
           </span>
-          <span id='I' class='error flex-row px-24'>
+          <span id='I' className='error flex-row px-24'>
             0
           </span>
-          <span id='G' class='error flex-row'>
+          <span id='G' className='error flex-row'>
             4
           </span>
         </div>
-        <p class='text-white/50 font-[Questrial] text-lg lg:text-2xl tracking-widest select-none z-20'>
+        <p className='text-white/50 font-[Questrial] text-lg lg:text-2xl tracking-widest select-none z-20'>
           Page not found
         </p>
-        <p class='text-white/50 font-[Questrial] px-4 lg:px-0 text-md text-center lg:text-left pt-4 lg:text-xl tracking-widest select-none z-20'>
+        <p className='text-white/50 font-[Questrial] px-4 lg:px-0 text-md text-center lg:text-left pt-4 lg:text-xl tracking-widest select-none z-20'>
           {' '}
           The page you are looking for does not exist or has been moved.
         </p>
         <Link
           href='/'
-          class='text-white/50 font-[Questrial] text-xl tracking-widest select-none underline decoration-white/50 z-20 pt-4'
+          className='text-white/50 font-[Questrial] text-xl tracking-widest select-none underline decoration-white/50 z-20 pt-4'
         >
           Go to home
         </Link>
