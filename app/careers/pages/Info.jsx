0fd4995c688b7915d8ae12c71bccbfe4100ad1dc
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-05-18 23:48:10 -0600

    🔁 Commitment changed

--- app/careers/pages/Info.jsx
+++ app/careers/pages/Info.jsx
@@ -3,17 +3,18 @@ import Image from 'next/image'
 export default function Gip() {
   return (
     <section>
-      <div className='md:pt-80 pb-8 px-16 overflow-hidden z-10'>
+      <div className='md:pt-80 pb-8 md:px-16 overflow-hidden z-10'>
         <div className='max-w-full'>
           <div className='lg:grid lg:grid-cols-12 mx-auto justify-center'>
             <article className='relative z-10 px-4 sm:px-6 sm pb-10 md:pb-0 pt-10 md:pt-0 lg:mb-0 lg:px-0 lg:text-left lg:flex col-span-6'>
               <div className='lg:py-24 px-4 sm:px-16'>
                 <h6 className='text-[burlywood] lg:text-left font-sans tracking-wide font-semibold text-lg'>
-                  Our Culture
+                  Our Commitment
                 </h6>
                 <p className='mt-3 font-[Questrial] sm:mx-auto lg:mx-0 max-w-lg lg:text-left text-xl sm:text-4xl text-white font-bold sm:mt-5'>
-                  Our commitment is based on the principles of learning,
-                  collaboration, passion and transparency of all our work.
+                  Our work is based on the principles of learning,
+                  collaboration, passion and transparency with all our work
+                  teams.
                 </p>
               </div>
             </article>
