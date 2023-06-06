4f6d98be4862638fe59b7a06b3a6cb1a478d7ff8
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-06-06 00:11:17 -0600

    ✅ hero component finished

---
+++ app/ceo/components/Hero.jsx
@@ -0,0 +1,13 @@
+import styles from './CEO.module.css'
+
+export default function Hero() {
+  return (
+    <section className={styles.CEO}>
+      <h1 className={styles.titleCEO}>Chief Executive Officer</h1>
+      <p className='text-gray-500 font-[Questrial] text-lg md:text-2xl text-center px-16'>
+        Find photos, biographies, and speech transcripts of Boxode executives
+        and board of directors members.
+      </p>
+    </section>
+  )
+}
