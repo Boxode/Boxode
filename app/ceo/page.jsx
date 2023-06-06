07b440b0a25e120332d2484d259d343069440a19
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-06-06 00:10:42 -0600

    🆕 CEO page added

---
+++ app/ceo/page.jsx
@@ -0,0 +1,11 @@
+import Header from '../careers/components/Header'
+import Hero from './components/Hero'
+
+export default function CEOPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
