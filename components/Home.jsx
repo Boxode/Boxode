979bcfb82e402be00cd545e20dd9005aadce3c62
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-06-23 22:32:35 -0600

    🔁 change the first info in the page

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,13 +7,15 @@ export default function Home() {
         <div className='mx-auto max-w-7xl px-4 sm:px-6'>
           <div className='lg:flex lg:justify-center lg:items-center lg:flex-col mx-auto lg:gap-16 justify-center'>
             <div className='relative z-10 px-4 mb-8 sm:px-6 sm:text-center sm:mb-16 lg:mb-0 lg:px-0 lg:flex col-span-6'>
-              <div className='lg:py-24'>
-                <p className='mt-4 mb-4 text-2xl lg:text-6xl h-full font-[SegoeBold] tracking-tight font-extrabold primary-title sm:mt-5 text-center'>
-                  Creando soluciones informáticas personalizadas y productos
-                  comerciales a pequeñas, medianas y grandes empresas.
+              <div className='lg:py-14'>
+                <p className='text-4xl lg:text-9xl lg:px-10 h-full font-[SegoeBold] tracking-tight font-extrabold primary-title text-center'>
+                  A better way to build software.
                 </p>
-                <p className='mt-3 font-[Questrial] text-center text-base sm:text-2xl text-neutral-200/50 sm:mt-5'>
-                  "Para de hablar y comienza a innovar."
+                <p className='mt-3 font-[Questrial] text-center text-base sm:text-2xl text-neutral-200/50'>
+                  Let your ideas flow.
+                </p>
+                <p className='mt-3 md:mb-6 font-[Questrial] text-center text-base sm:text-2xl text-neutral-200/50 sm:mt-2'>
+                  We make them come true.
                 </p>
               </div>
             </div>
