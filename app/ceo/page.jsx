5c014abd68dc5fdb578b0030781c2ee168d4d6af
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-06-25 18:51:31 -0600

    🔁 Routes of components changed

--- app/ceo/page.jsx
+++ app/ceo/page.jsx
@@ -1,11 +1,13 @@
-import Header from '../careers/components/Header'
+import Header from '@/components/Header'
 import Hero from './components/Hero'
+import Footer from '@/components/Footer'
 
 export default function CEOPage() {
   return (
     <>
       <Header />
       <Hero />
+      <Footer />
     </>
   )
 }
