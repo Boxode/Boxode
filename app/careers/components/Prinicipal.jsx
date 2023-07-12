a91ac19b56df1de9fb0a04074218df805ce04334
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-07-12 00:31:19 -0600

    ✨ Animate world map dotted added

--- app/careers/components/Prinicipal.jsx
+++ app/careers/components/Prinicipal.jsx
@@ -1,15 +1,15 @@
 'use client'
 
 import style from './Careers.module.css'
-// import Image from 'next/image'
-import World from './World'
+import Image from 'next/image'
+// import World from './World'
 import { Grid, Button } from '@nextui-org/react'
 
-export default function Principal () {
+export default function Principal() {
   return (
     <section className={style.Careers}>
       <div className='flex justify-center items-center flex-col'>
-        <h1 className='text-white text-center pt-6 md:pt-0 text-9xl md:mt-40 select-none font-[PoppinsBold] primary-title'>
+        <h1 className='text-white text-center pt-6 md:pt-0 text-8xl md:text-9xl md:mt-40 select-none font-[PoppinsBold] primary-title'>
           Join our team
         </h1>
         <p className='text-gray-300 text-lg md:text-2xl font-[Satoshi] drop-shadow-md mb-8 text-center mt-2'>
@@ -46,16 +46,16 @@ export default function Principal () {
             </Grid>
           </Grid.Container>
         </div>
-        <div className='absolute -z-20 md:mt-32 inline-flex justify-center items-center'>
+        {/* <div className='absolute -z-20 md:mt-32 inline-flex justify-center items-center'>
           <World />
-        </div>
-        {/* <Image
-          src='/svg/world_dotted_map.svg'
+        </div> */}
+        <Image
+          src='/World-Map-Heat-Map.gif'
           alt='World map'
           width={1200}
           height={1200}
-          className='absolute -z-20 md:mt-80 opacity-40 md:opacity-20'
-        /> */}
+          className='absolute -z-20 md:mt-80 opacity-40 md:opacity-25 grayscale'
+        />
       </div>
     </section>
   )
