76b87c73d4195081ca7cf59fd2685017cc901e84
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-08-07 23:57:58 -0600

    feat: Code of conduct page added.

---
+++ app/[locale]/oss/coc/page.jsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export default function Coc() {
+  return (
+    <main>
+      <Header />
+      <h1 className='text-white text-6xl text-center pt-32 font-[PoppinsBold]'>
+        Code of Conduct
+      </h1>
+      <Footer />
+    </main>
+  )
+}
