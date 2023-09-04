f0a0ec00251d65cbda5433c8015f4d7b077c300e
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-09-03 23:44:12 -0600

    Code of coduct page added

---
+++ app/[locale]/oss/coc/page.jsx
@@ -0,0 +1,16 @@
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export default function Coc() {
+  return (
+    <main>
+      <Header />
+      <div>
+        <h1 className='text-white text-6xl text-center pt-32 font-[PoppinsBold]  primary-title primary-title-hero'>
+          Code of Conduct
+        </h1>
+      </div>
+      <Footer />
+    </main>
+  )
+}
