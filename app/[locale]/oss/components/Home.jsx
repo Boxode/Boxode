bb674eef05885d6d9181842e0a04577b0bbf67ef
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-10-05 11:19:56 -0600

    fix: description responsiveness fixed

--- app/[locale]/oss/components/Home.jsx
+++ app/[locale]/oss/components/Home.jsx
@@ -2,16 +2,15 @@
 
 import styles from './OSS.module.css'
 import { useTranslations } from 'next-intl'
-import Balancer from 'react-wrap-balancer'
 
 export default function HomeOSS() {
   const t = useTranslations('OSS')
   return (
     <section className={styles.OSS}>
       <h1 className={styles.titleOSS}>{t('title')}</h1>
-      <Balancer className='text-gray-500 font-[GilroyRegular] text-lg md:text-2xl text-center px-16'>
+      <p className='text-gray-500 font-[GilroyRegular] text-lg md:text-2xl text-center px-16'>
         {t('subtitle')}
-      </Balancer>
+      </p>
     </section>
   )
 }
