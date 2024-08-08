2df0f77bb7fc30d9e4b00742e94e789b4f9684d5
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-08-08 11:18:05 -0600

    fix: vacants available section fixed

--- app/[locale]/careers/components/OpenP.jsx
+++ app/[locale]/careers/components/OpenP.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable no-tabs */
 export default function OpenP() {
   return (
     <section>
       <div className='text-white pt-10 md:pt-24'>
-        <p className='font-[GilroyRegular] pt-4 px-4 md:px-0 text-lg md:text-xl text-center'>
+				<p className='font-[GilroyRegular] pt-4 px-4 md:px-0 text-lg md:text-xl text-center pb-28'>
           Thank you for being interested in working with us, but there are
           currently no vacancies available.
         </p>
